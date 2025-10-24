8c53e6b272c99e8847092c812d2784997c4d193d
Hoangvu2k12 <187742656+Hoangvu2k12@users.noreply.github.com>
2025-10-24 12:14:06 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,6 +1,6 @@
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
 
-local allowedUsers = {"LV_NM12","LV_NM7"}
+local allowedUsers = {"LV_NM12","LV_NM5"}
 local playerName = game.Players.LocalPlayer.Name
 local bypassKey = table.find(allowedUsers, playerName) ~= nil
 
