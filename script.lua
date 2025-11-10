f99d067d89874c964f007a82af64b4ca2f6725b1
Hoangvu2k12 <187742656+Hoangvu2k12@users.noreply.github.com>
2025-11-10 18:34:36 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,9 +1,5 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local allowedUsers = {"hdhjs537534"}
-local playerName = game.Players.LocalPlayer.Name
-local bypassKey = table.find(allowedUsers, playerName) ~= nil
-
 local Window = Rayfield:CreateWindow({
    Name = "MINHTA V1.6",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
